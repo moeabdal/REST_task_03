e86a6de553b67e156e2e5af07bd063bb72fa88a8
moeabdal <mohammed.h.abdal@gmail.com>
2019-07-24 19:00:37 +0300

    Finished task

--- task_1/urls.py
+++ task_1/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from flights import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +29,7 @@ urlpatterns = [
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
     path('booking/<int:booking_id>/update/', views.UpdateBooking.as_view(), name="update-booking"),
     path('booking/<int:booking_id>/cancel/', views.CancelBooking.as_view(), name="cancel-booking"),
+    path('book/<int:flight_id>', views.FlightBooking.as_view(), name="book-flight"),
+    path('login/', TokenObtainPairView.as_view(), name="login"),
+    path('refresh/', TokenRefreshView.as_view(), name="token-refresh")
 ]
